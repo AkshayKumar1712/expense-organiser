b6f21ffc63f5531293e7f0b87368d2d42ca6a944
halifage <halifsarki@gmail.com>
2018-10-28 21:20:14 +0200

    added additional checks for various FNB messages

--- app/src/main/java/protect/expenseorganiser/SmsListener.java
+++ app/src/main/java/protect/expenseorganiser/SmsListener.java
@@ -4,8 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PatternMatcher;
 import android.provider.Telephony;
 import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SmsListener extends BroadcastReceiver {
 
@@ -14,16 +19,37 @@ public class SmsListener extends BroadcastReceiver {
         String messageBody = "";
         if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
             for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
-               messageBody = smsMessage.getMessageBody();
+                messageBody = smsMessage.getMessageBody();
             }
         }
 
-        String value;
-        String name;
+        String value = "";
+        String name = "";
 
-        if(messageBody.startsWith("FNB :-) R")){
-            value = messageBody.substring(messageBody.indexOf("R") + 1, messageBody.indexOf("R") + 7);
-            name = messageBody.substring(messageBody.indexOf("@") + 2, messageBody.indexOf(" from"));
+        Pattern pattern = Pattern.compile("([0-9.]+)");
+        Matcher matcher;
+
+        if (messageBody.contains("FNB :-)")) {
+            if (messageBody.contains("reserved")) {
+                matcher = pattern.matcher(messageBody.split("from")[0].trim());
+                while (matcher.find()) {
+                    value = matcher.group(0);
+                }
+                name = messageBody.split("@")[1].split("from")[0].trim();
+            } else if (messageBody.contains("paid")) {
+                matcher = pattern.matcher(messageBody);
+                while (matcher.find()) {
+                    value = matcher.group(0);
+                }
+                name = messageBody.split("Ref")[1].split(".")[0].trim();
+            }else if (messageBody.contains("withdrawn")){
+                matcher = pattern.matcher(messageBody.split("from")[0].trim());
+                while (matcher.find()) {
+                    value = matcher.group(0);
+                }
+                name = "Withdrawal @ " + messageBody.split("@")[1].split("Avail")[0].trim();
+            }
+
             Intent i = new Intent(context, TransactionViewActivity.class);
             final Bundle b = new Bundle();
             b.putInt("type", 1);
